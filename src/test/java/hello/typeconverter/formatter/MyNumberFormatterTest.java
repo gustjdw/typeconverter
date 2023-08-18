4f6dd9dad4eab8e78b4439ddffbd3d27118760f4
gustjdw <johs710@gmail.com>
2023-08-18 21:04:02 +0900

    포맷터를 지원하는 컨버전 서비스

--- src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
+++ src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -1,13 +1,11 @@
 package hello.typeconverter.formatter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MyNumberFormatterTest {
 
